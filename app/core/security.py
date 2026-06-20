e8357abb71433a2294d67374a5ba1d7090137be5
amna-nr <amnanoor5778@gmail.com>
2026-06-20 20:20:15 +0500

    feat: add security utilities, JWT and password hashing

---
+++ app/core/security.py
@@ -0,0 +1,42 @@
+from app.core.config import settings
+
+import bcrypt 
+import secrets
+
+from datetime import datetime, timedelta, timezone
+from jose import jwt, JWTError
+from fastapi import HTTPException
+from starlette import status
+
+
+ACCESS_TOKEN_EXPIRES_MINUTES = settings.ACCESS_TOKEN_EXPIRES_MINUTES
+REFRESH_TOKEN_EXPIRES_DAYS = settings.REFRESH_TOKEN_EXPIRES_DAYS
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+
+
+def hash_password(password: str):
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+    
+def check_password(plain: str, hashed: str) -> bool:
+    return bcrypt.hashpw(plain.encode("utf-8"), hashed.encode("utf-8"))
+      
+def generate_access_token(data: dict):
+    to_encode = data.copy()
+    expires = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
+    to_encode.update({"exp" : expires})
+    access_token = jwt.encode(to_encode, SECRET_KEY, algorithms=[ALGORITHM])
+    return access_token
+
+def decode_jwt_token(token: str):
+    try: 
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token"
+        )
+    return payload
+
+def generate_refresh_token():
+    return secrets.token_urlsafe()
