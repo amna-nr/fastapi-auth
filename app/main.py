970bc2a1ef0814ee7bb61c73261647646c67967c
amna-nr <amnanoor5778@gmail.com>
2026-06-22 00:29:27 +0500

    feat: add main app setup with lifespan

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.core.redis import redis_client
+from app.core.logging import setup_logging, logger
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    setup_logging()
+    logger.info("app_started")
+    await redis_client.ping()
+    logger.info("redis_connected")
+    yield
+    await redis_client.aclose()
+    logger.info("app_shutdown")
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def home():
+    return {"message": "welcome to fastapi auth"}
