0a337121f08feaec4cab893384f3448d5ed074e9
amna-nr <amnanoor5778@gmail.com>
2026-06-23 22:35:00 +0500

    feat: add auth router to main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+
 from app.core.redis import redis_client
 from app.core.logging import setup_logging, logger
+from app.routes.auth import router as auth_router 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,6 +16,8 @@ async def lifespan(app: FastAPI):
     logger.info("app_shutdown")
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
