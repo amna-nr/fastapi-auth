04a30c608bcc08cf93fb751258a6ca8d361ed419
amna-nr <amnanoor5778@gmail.com>
2026-06-23 17:07:57 +0500

    fix: rename user model file

---
+++ app/models/user.py
@@ -0,0 +1,16 @@
+from app.core.database import Base
+
+from sqlalchemy import Column, String, Boolean, TIMESTAMP, func
+from sqlalchemy.dialects.postgresql import UUID
+from uuid import uuid4
+
+
+class User(Base):
+    __tablename__="users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    email = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False) 
+    role = Column(String, default="member" )
+    is_verified = Column(Boolean, default=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
